25a7888cbb9a6d2683f3a855f4459f2962e6b1c9
Ho Guo Xian <hogouxian@gmail.com>
2018-01-15 17:16:53 +0800

    added -f flag for find the duplicate

--- src/main.c
+++ src/main.c
@@ -1,20 +1,50 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "fileDuplication.h"
 
 int main(int args, char *kwargs[])
 {
-    printf("input : %s\n", kwargs[1]);
-    char resultPath[256];
-    if (args < 2)
+    int c;
+    int fFlag = 0, dFlag = 0;
+    char *path = NULL;
+    while ((c = getopt(args, kwargs, "fd:")) != -1)
     {
-        printf("error! Path need to be specified!");
+        switch(c){
+            case 'f':
+                fFlag = 1;
+                break;
+            case 'd':
+                dFlag = 1;
+                break;
+            case '?':
+                if (optopt == 'f')
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint(optopt))
+                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf(stderr,
+                            "Unknown option character `\\x%x'.\n",
+                            optopt);
+                return 1;
+            default:
+                abort();
+        }
     }
+
+    path = kwargs[optind];
+    if(fFlag){
+        DuplicationList dupL = findDuplicate(path);
+        printf("--------------------------\n");
+        printf("| Duplication File Finder |\n");
+        printf("--------------------------\n");
+        printf("number of duplication found : %d\n\n", dupL.numberOfDuplication);
+        listAllDuplication(dupL);
+    }
+    if(dFlag)
+        printf("d flag is set\n");
     
-    DuplicationList dupL = findDuplicate(kwargs[1]);
-    printf("--------------------------\n");
-    printf("| Duplication File Finder |\n");
-    printf("--------------------------\n");
-    printf("number of duplication found : %d\n\n", dupL.numberOfDuplication);
-    listAllDuplication(dupL);
-    return 0;
+    return 1;
 }
 
